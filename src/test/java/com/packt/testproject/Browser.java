3c0026f94b693efcf69f244ac5544b25d6f0c112
Bojan Popovic <bojan.popovic@devtechgroup.com>
2019-09-04 10:52:21 +0200

    playing with jenkis

--- src/test/java/com/packt/testproject/Browser.java
+++ src/test/java/com/packt/testproject/Browser.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 
-import java.io.File;
-
 
 public class Browser {
 
@@ -30,8 +28,8 @@ public class Browser {
 
             ClassLoader classLoader = getClass().getClassLoader();
             //String path=  new File(classLoader.getResource("chromedriver").getFile()).getPath();
-            System.setProperty("webdriver.chrome.driver", new File(classLoader.getResource("chromedriver").getFile()).getPath().replace("%20", " "));
-            //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+            //System.setProperty("webdriver.chrome.driver", new File(classLoader.getResource("chromedriver").getFile()).getPath().replace("%20", " "));
+            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("chrome.switches", "--disable-extensions", "test-type", "start-maximized");
